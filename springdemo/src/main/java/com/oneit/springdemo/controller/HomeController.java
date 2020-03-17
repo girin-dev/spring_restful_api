a9785aad7c87b7cd6359a29ff461c6374126a7cf
kwak.kuwon <kwak.kuwon@joins.com>
2020-03-17 13:42:47 +0900

    security & jwt

--- springdemo/src/main/java/com/oneit/springdemo/controller/HomeController.java
+++ springdemo/src/main/java/com/oneit/springdemo/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.oneit.springdemo.controller;
 
+import com.oneit.springdemo.service.SecurityService;
+import com.oneit.springdemo.service.SecurityServiceImpl;
 import com.oneit.springdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -19,6 +20,23 @@ public class HomeController {
     public Map<String,String> home() {
         Map<String,String> res = this.userService.getMessage();
         return res;
+    }
+
+    @Autowired
+    private SecurityService securityService;
+
+    @GetMapping("security/generate/token")
+    public Map<String, Object> generateToken(@RequestParam String subject){
+        String token = securityService.createToken(subject, 1000*60*60);
+        Map<String, Object> map = new HashMap<>();
+        map.put("userid", subject);
+        map.put("token", token);
+        return map;
+    }
+    @GetMapping("security/get/subject")
+    public String getSubject(@RequestParam String token){
+        String subject = securityService.getSubject(token);
+        return subject;
     }
 
 }
